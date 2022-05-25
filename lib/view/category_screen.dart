693dba3ef6dffee29988262d323fa22ba4247932
ZulhusniAzhar <92666477+ZulhusniAzhar@users.noreply.github.com>
2022-05-26 00:52:41 +0800

    ver1

--- lib/view/category_screen.dart
+++ lib/view/category_screen.dart
@@ -1,129 +1,6 @@
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import '../widget/widget.dart';
-import 'dashboard.dart';
-
-class CategoryScreen extends StatefulWidget {
-  @override
-  _CategoryScreenState createState() => _CategoryScreenState();
-}
-
-class _CategoryScreenState extends State<CategoryScreen> {
-  
-  late final Stream<QuerySnapshot> quizzes =
-      FirebaseFirestore.instance.collection('QuizList').snapshots();
-
-
-  Widget quizList() {
-    return Container(
-      child: Column(
-        children: [
-          StreamBuilder<QuerySnapshot>(
-            stream: quizzes,
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return const Text('Something went wrong.');
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text('Loading');
-              }
-
-              final data = snapshot.requireData;
-              return ListView.builder(
-                shrinkWrap: true,
-                physics: const ClampingScrollPhysics(),
-                itemCount: data.size,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const DashboardScreen()));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 24),
-                      height: 150,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Stack(
-                          children: [
-                            Image.network(
-                              '${data.docs[index]['quizImgurl']}',
-                              fit: BoxFit.cover,
-                              width: MediaQuery.of(context).size.width,
-                            ),
-                            Container(
-                              color: Colors.black26,
-                              child: Center(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      '${data.docs[index]['quizTitle']}',
-                                      style: const TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                    const SizedBox(
-                                      height: 4,
-                                    ),                                 
-                                    const SizedBox(
-                                      height: 4,
-                                    ),
-                                    Text(
-                                      '${data.docs[index]['quizDesc']}',
-                                      style: const TextStyle(
-                                          fontSize: 13,
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.w500),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          )
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: AppLogo(),
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-
-      ),
-      body: quizList(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const DashboardScreen()));
-        },
-      ),
-    );
-  }
-}
-
-
-/*import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -158,8 +35,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
               final data=snapshot.requireData;
               return ListView.builder(
                 shrinkWrap: true,
-                physics: const ClampingScrollPhysics(),
-                itemCount: data.size,
+                physics: const AlwaysScrollableScrollPhysics(),
+                itemCount: 3 ,// data.size,
                 itemBuilder:(context,index){
                   return QuizTile( 
                         noOfQuestions: 
@@ -178,7 +55,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
                         '${data.docs[index]['quizId']}',
                           //snapshot.data.documents[index].data["quizId"],
                   );  
-              },);
+              });
             },
           )
         ],
@@ -238,8 +115,8 @@ class QuizTile extends StatelessWidget {
             context, MaterialPageRoute(builder: (context) => const DashboardScreen()));
       },
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        height: 150,
+        padding: const EdgeInsets.symmetric(horizontal: 30,vertical: 5),
+        height: 130,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(8),
           child: Stack(
@@ -257,8 +134,9 @@ class QuizTile extends StatelessWidget {
                     children: [
                       Text(
                         title,
+                        textAlign: TextAlign.center,
                         style: const TextStyle(
-                            fontSize: 18,
+                            fontSize: 24,
                             color: Colors.white,
                             fontWeight: FontWeight.w500),
                       ),
@@ -267,6 +145,7 @@ class QuizTile extends StatelessWidget {
                       ),
                       Text(
                         description,
+                        textAlign: TextAlign.center,
                         style: const TextStyle(
                             fontSize: 13,
                             color: Colors.white,
@@ -282,4 +161,4 @@ class QuizTile extends StatelessWidget {
       ),
     );
   }
-}*/
+}
--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -4,9 +4,9 @@ class AppLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RichText(
-      text: TextSpan(
+      text: const TextSpan(
         style: TextStyle(
-            fontSize: 22
+            fontSize: 25,
         ),
         children: <TextSpan>[
           TextSpan(text: 'My', style: TextStyle(fontWeight: FontWeight.w600
