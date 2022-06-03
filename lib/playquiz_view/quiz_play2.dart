9cd32938f1d743df64274606d4b3934cbe487f6d
manzajp <manzajp.23@outlook.com>
2022-06-04 00:17:31 +0800

    can use dropdown answer now

--- lib/playquiz_view/quiz_play2.dart
+++ lib/playquiz_view/quiz_play2.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:map_proj/new_api/test_api.dart';
+import 'package:map_proj/new_model/question_model.dart';
 import 'package:map_proj/new_model/test_model.dart';
 import 'package:map_proj/new_notifier/test_notifier.dart';
 import 'package:map_proj/playquiz_view/quiz_play_widgets.dart';
@@ -12,88 +13,86 @@ import 'package:provider/provider.dart';
 class QuestionPlay extends StatefulWidget {
   const QuestionPlay({Key? key, required this.testName}) : super(key: key);
   final String testName;
-  
+
   @override
   State<QuestionPlay> createState() => _QuestionPlay();
 }
 
-
-
-
 class _QuestionPlay extends State<QuestionPlay> {
   final _formKey = GlobalKey<FormState>();
 
-  String optionSelected="";
-  bool answerTap =false;
-  int scorea=0;
-  int scoreGet=0;
+  String optionSelected = "";
+  bool answerTap = false;
+  int scorea = 0;
+  int scoreGet = 0;
   int totalScore = 0;
-  String optionChose="";
-  int index=0;
+  String optionChose = "";
+  int index = 0;
   //1st :aku nak create dropdownValue as list so each dropdown can display its own dropdownValue(variable utk simpan jawapan yang dia pilih)
-  List<String?> dropdownValue[i]; //code asal: String? dropdownValue;
+  // List<String?> dropdownValue = []; //code asal: String? dropdownValue;
+  List<String?> dropdownValueAnswer = [];
 
-  
   @override
   Widget build(BuildContext context) {
     TestNotifier testNotifier =
         Provider.of<TestNotifier>(context, listen: false);
     TestModel _currentTestModel = testNotifier.currentTestModel;
-    final testNameEditingController =
-        TextEditingController(text: _currentTestModel.quizTitle);
+    // final testNameEditingController =
+    //     TextEditingController(text: _currentTestModel.quizTitle);
 
     Widget questionField(TestModel _currentTestModel) {
       return FutureBuilder(
-      future: getQuestionFuture(_currentTestModel),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        }
-        return ListView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          shrinkWrap: true,
-          itemCount: _currentTestModel.questions?.length ?? 0,
-          itemBuilder: ((context, i) => SizedBox(
-            width:400,
-            child: ListTile(
-                  title: Text(
-                    '\nQ${i + 1} ${_currentTestModel.questions![i].question}\n',
-                    style:
-                    TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.8)),
-                    
+          future: getQuestionFuture(_currentTestModel),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (dropdownValueAnswer.isEmpty) {
+              dropdownValueAnswer = _currentTestModel.questions!
+                  .map((e) => e.option![0] as String)
+                  .toList();
+            }
+
+            return ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: _currentTestModel.questions?.length ?? 0,
+                itemBuilder: ((context, i) {
+                  List<QuestionModel>? dropdownValueQuestion =
+                      _currentTestModel.questions;
+
+                  return SizedBox(
+                    width: 400,
+                    child: ListTile(
+                      title: Text(
+                        '\nQ${i + 1} ${dropdownValueQuestion![i].question}\n',
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.black.withOpacity(0.8)),
+                      ),
+                      subtitle: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 5, vertical: 10),
+                        child: DropdownButton(
+                          value: dropdownValueAnswer[i],
+                          items: dropdownValueQuestion[i]
+                              .option!
+                              .map((e) => DropdownMenuItem(
+                                    child: Text(e as String),
+                                    value: e,
+                                  ))
+                              .toList(),
+                          onChanged: (String? value) {
+                            setState(() {
+                              dropdownValueAnswer.insert(i, value);
+                              dropdownValueAnswer.removeAt(i + 1);
+                            });
+                          },
+                        ),
+                      ),
                     ),
-                  subtitle:  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-                    child: DropdownButton(
-                      //2nd:aku nak dropdownValue keluar ikut index lpas declared as List (index tu kalau bleh nak ikut index soalan)
-                        value: dropdownValue[i], //code asal: value:dropdownValue,
-                        icon: const Icon(Icons.arrow_downward),
-                        style: const TextStyle(color:  Color.fromARGB(255, 7, 68, 97)),
-                        underline: Container(
-                        height: 2,
-                        color: const Color.fromARGB(255, 27, 183, 255),
-                      ),
-                      
-                        items: _currentTestModel.questions![i].option!.map((e) {
-                          return DropdownMenuItem(
-                            value: e,
-                            child: Text(e),
-                            
-                          );
-                        }).toList(),
-                        onChanged: (val) async {
-                            setState(() => 
-                            //3rd: bila ada perubahan, dia store dalam each index dropdownValue tu (index tu nak guna index soalan)
-                        dropdownValue[i] = val as String); //code asal: dropdownValue= val as String);
-                        } 
-                        //4rd: nak guna for loop untuk loop balik semula dropdownValue[] untuk kira each score by using if(a),else if(b)etc
-                      ),
-                  ),          
-                  ),
-          )),
-        );
-      },
-      );
+                  );
+                }));
+          });
     }
 
     return Scaffold(
@@ -104,9 +103,7 @@ class _QuestionPlay extends State<QuestionPlay> {
           FloatingActionButton(
             backgroundColor: const Color.fromARGB(255, 27, 183, 255),
             child: const Icon(Icons.done),
-            onPressed: () {
-
-            },
+            onPressed: () {},
           ),
         ],
       ),
@@ -139,7 +136,6 @@ class _QuestionPlay extends State<QuestionPlay> {
                   children: <Widget>[
                     const SizedBox(height: 5),
                     questionField(_currentTestModel),
-                   
                   ],
                 ),
               ),
